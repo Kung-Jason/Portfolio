8775341582b0407ee5a96712f02cf13c044b233f
Jason <jsahsionne@gmail.com>
2022-07-25 02:43:05 +0800

    測試java迴圈

--- js/all.js
+++ js/all.js
@@ -1,19 +1,26 @@
+let $opacity = .1;
 $(document).ready(function () {
         // $('.desc').find('h1').fadeIn('slow');
         console.log(0);
         // $('.init').css(display, flex);
         $('.desc').fadeIn(3000);
-        let $opacity = 1;
-        // $('h2').css('opacity', $opacity);
-        if ($opacity != 0) {
-                $opacity -= 0.1;
+        $('h2').fadeIn(FadeIn);
+       
+      
+       
+       
+});
+function FadeIn() {
+        if ($opacity != 1) {
+                $opacity += 0.1;
                 $('h2').css('opacity', $opacity);
+                console.log($opacity);
         } else {
                 $opacity = 1;
                 $('h2').css('opacity', $opacity);
+                console.log($opacity);
         }
-        // $('desc').find('h2').fadeToggle(1500);   
-});
+  }
 // function fadeIn() {
 //         $('.init').css(display, flex);
 //         $('.init').fadeIn(5000);
