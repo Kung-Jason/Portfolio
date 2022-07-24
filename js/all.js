f18ce7fcbff37ca5b646b1d6e7d47f5b477c2ae2
Jason <jsahsionne@gmail.com>
2022-07-24 23:24:33 +0800

    封面動畫ok但h2失敗

--- js/all.js
+++ js/all.js
@@ -1,3 +1,20 @@
-$('.init').click(function () { 
-        // $('.init').find('.desc').
+$(document).ready(function () {
+        // $('.desc').find('h1').fadeIn('slow');
+        console.log(0);
+        // $('.init').css(display, flex);
+        $('.desc').fadeIn(3000);
+        let $opacity = 1;
+        // $('h2').css('opacity', $opacity);
+        if ($opacity != 0) {
+                $opacity -= 0.1;
+                $('h2').css('opacity', $opacity);
+        } else {
+                $opacity = 1;
+                $('h2').css('opacity', $opacity);
+        }
+        // $('desc').find('h2').fadeToggle(1500);   
 });
+// function fadeIn() {
+//         $('.init').css(display, flex);
+//         $('.init').fadeIn(5000);
+// }
